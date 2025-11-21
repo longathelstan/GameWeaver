3b36f1e3ffe40a42067b55ac61a5964aa85a6c2a
tlowng <lowng@thptchuyenhatinh.edu.vn>
2025-11-21 15:08:31 +0700

    ì

--- api-server/src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const generatorController = require('../controllers/generator.controller');
-
-const router = express.Router();
-
-router.post('/generate', generatorController.generateContent);
-
-module.exports = router;
